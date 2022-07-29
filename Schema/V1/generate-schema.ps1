8b7db9e50a25ecbe973e5f573f30060a07b87ce4
erltor <erltor@acos.no>
2022-07-29 15:24:39 +0200

    added skript

---
+++ Schema/V1/generate-schema.ps1
@@ -0,0 +1,19 @@
+#generate-schema.ps1
+$xsd = 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\xsd.exe'
+xsd /c /p /nologo /n:Acos.Felles.Fiks.IO.Models.Schema.Fiks.V1 `
+    .\metadatakatalog.xsd `
+    .\no.ks.fiks.arkiv.v1.arkivering.arkivmelding.xsd `
+    .\arkivstruktur.xsd `
+    .\no.ks.fiks.arkiv.v1.arkivering.arkivmelding.kvittering.xsd `
+    .\arkivstrukturMinimum.xsd `
+    .\arkivstrukturNoekler.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.dokumentfil.hent.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.journalpost.hent.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.journalpost.hent.resultat.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.mappe.hent.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.mappe.hent.resultat.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.sok.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.sok.resultat.minimum.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.sok.resultat.noekler.xsd `
+    .\no.ks.fiks.arkiv.v1.innsyn.sok.resultat.utvidet.xsd `
+    /out:C:\Kode\git\Acos.Felles.Fiks.IO.Integrasjoner\Acos.Felles.Fiks.IO\Models\Schema\Arkivmelding\Fiks\V1
